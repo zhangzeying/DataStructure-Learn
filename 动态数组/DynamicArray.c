b6c42b1c084d9672c3145819096cd0c92022616f
张泽瀛 <zhangzy@weipaitang.com>
2018-10-14 18:07:34 +0800

    动态数组基本框架搭建

--- 动态数组/DynamicArray.c
+++ 动态数组/DynamicArray.c
@@ -7,3 +7,38 @@
 //
 
 #include "DynamicArray.h"
+
+//初始化动态数组
+ZZYDynamicArray *initDynamicArray(void) {
+    return NULL;
+}
+
+//插入元素
+void insertObject(ZZYDynamicArray *array, void *data) {
+    
+}
+
+//根据位置删除元素
+void removeObjectByIndex(ZZYDynamicArray *array, int index) {
+    
+}
+
+//根据值删除元素
+void removeObjectByValue(ZZYDynamicArray *array, void *data) {
+    
+}
+
+//查找元素
+void *findObject(ZZYDynamicArray *array, void *data) {
+    return NULL;
+}
+
+//打印数组
+void printDynamicArray(ZZYDynamicArray *array) {
+    
+}
+
+//释放动态数组内存
+void freeDynamicArray(ZZYDynamicArray *array) {
+    
+}
--- 动态数组/DynamicArray.h
+++ 动态数组/DynamicArray.h
@@ -11,4 +11,26 @@
 
 #include <stdio.h>
 
+//动态数组的结构体定义
+typedef struct ZZYDynamicArray {
+    void *pAddr; //存放数据的首地址
+    int size; //当前有多个元素
+    int capacity; //数组最多能容纳的元素个数
+}ZZYDynamicArray;
+
+//初始化动态数组
+ZZYDynamicArray *initDynamicArray(void);
+//插入元素
+void insertObject(ZZYDynamicArray *array, void *data);
+//根据位置删除元素
+void removeObjectByIndex(ZZYDynamicArray *array, int index);
+//根据值删除元素
+void removeObjectByValue(ZZYDynamicArray *array, void *data);
+//查找元素
+void *findObject(ZZYDynamicArray *array, void *data);
+//打印数组
+void printDynamicArray(ZZYDynamicArray *array);
+//释放动态数组内存
+void freeDynamicArray(ZZYDynamicArray *array);
+
 #endif /* DynamicArray_h */
